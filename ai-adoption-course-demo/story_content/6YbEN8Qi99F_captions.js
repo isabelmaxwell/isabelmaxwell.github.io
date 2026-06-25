14da5c377017d308f5851fdf0e24d095e33512f5
isabelmaxwell <isabel.n.maxwell@gmail.com>
2026-06-25 14:38:29 -0400

    Add AI Adoption Course Demo

---
+++ ai-adoption-course-demo/story_content/6YbEN8Qi99F_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6YbEN8Qi99F_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind%3A%20captions%0D%0ASource%3A%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version%3A%203.117.36909.0%0D%0A%0D%0A00%3A00%3A00.075%20--%3E%2000%3A00%3A02.711%0D%0AAct%3A%20Implement%20AI%20projects%20and%20solutions.%0D%0A%0D%0A"}]})
---
+++ ai-adoption-course-demo/story_content/triggers.js
@@ -0,0 +1,76 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5xGjl6AH0EF":
+        Script1();
+        break;
+      case "6PeFnr3w1YI":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5eMcCgqzFac');
+const duration = 2250;
+const easing = 'ease-out';
+const id = '5rJ8jp5jwyL';
+const pulseAmount = 0.07;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('5neelg5E9A0');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6C0Y4TR7i45';
+const pulseAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
